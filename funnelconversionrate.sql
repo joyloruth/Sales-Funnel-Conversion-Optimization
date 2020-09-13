013497ab8b24f21a05473f00244de17446a952c3
Joy LoRuth <46979334+joyloruth@users.noreply.github.com>
2020-09-12 19:14:51 -0700

    Update funnelconversionrate.sql

--- funnelconversionrate.sql
+++ funnelconversionrate.sql
@@ -1,14 +1,29 @@
 SELECT COUNT(*)
 FROM user_table;
 
+# COUNT(*)
+	90400
+
 SELECT COUNT(*)
 FROM home_page_table;
+
+# COUNT(*)
+	90400
 
 SELECT COUNT(*)
 FROM search_page_table;
 
+# COUNT(*)
+	45200
+  
 SELECT COUNT(*)
 FROM payment_page_table;
 
+# COUNT(*)
+	6030
+
 SELECT COUNT(*)
 FROM payment_confirmation_table;
+
+# COUNT(*)
+	452
