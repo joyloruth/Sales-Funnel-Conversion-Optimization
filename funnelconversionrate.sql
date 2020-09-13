6f17afb42c5e0822280caf47278c72e9ac352e44
Joy LoRuth <46979334+joyloruth@users.noreply.github.com>
2020-09-12 17:52:11 -0700

    Create funnelconversionrate.sql

---
+++ funnelconversionrate.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*)
+FROM user_table;
+
+SELECT COUNT(*)
+FROM home_page_table;
+
+SELECT COUNT(*)
+FROM search_page_table;
+
+SELECT COUNT(*)
+FROM payment_page_table;
+
+SELECT COUNT(*)
+FROM payment_confirmation_table;
